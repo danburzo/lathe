7d8006eb876f4089a2e04e6bcd139155ff6316e2
Dan Burzo <danburzo@gmail.com>
2019-07-16 11:30:35 +0300

    Menu locations

--- functions.php
+++ functions.php
@@ -27,9 +27,65 @@ Timber::$dirname = 'templates';
 class LatheSite extends Timber\Site {
 
 	function __construct() {
+
+		add_action('after_setup_theme', array($this, 'configure_theme'));
+
+		/*
+			The theme defines custom menu locations
+		*/
+		$menus = array(
+			'main-menu' => __('Main Menu', 'lathe'),
+			'footer-menu' => __('Footer Menu', 'lathe'),
+			'social-menu' => __('Social Menu', 'lathe')
+		);
+
+		/* Register menu locations */
+		add_action('init', function() use ($menus) {
+			register_nav_menus($menus);
+		});
+
+		add_filter('timber_context', function($context) use ($menus) {
+			// Pagination
+			$context['pagination'] = Timber::get_pagination();
+
+			// Main menu
+			$context['menu'] = new Timber\Menu('main-menu');
+
+			// All menus
+			$context['menus'] = [];
+			foreach(array_keys($menus) as $key) {
+				$context['menus'][$key] = new Timber\Menu($key);
+			}
+
+			return $context;
+		});
 		
+		parent::__construct();
+	}
+
+	/*
+		Configure the theme
+		-------------------
+
+		Setup the theme's capabilities and 
+		attach hooks to relevant filters and actions.
+	 */
+	function configure_theme() {
+
+		load_theme_textdomain('lathe', get_template_directory() . '/languages');
+
+		/*
+			Enable support for post formats.
+		 */
 		add_theme_support('post-formats');
+
+		/*
+			Enable support for post thumbnails on posts and pages.
+			
+			https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
 		add_theme_support('post-thumbnails');
+
 		add_theme_support('menus');
 		add_theme_support('html5', array(
 			'comment-list', 
@@ -39,27 +95,30 @@ class LatheSite extends Timber\Site {
 			'caption'
 		));
 
-		add_action('init', function() {
-			register_nav_menus(
-				array(
-					'main-menu' => __('Main Menu', 'lathe'),
-					'footer-menu' => __('Footer Menu', 'lathe')
-				)
-			);
+		/*
+			Add default posts and comments RSS feed links to head.
+		 */
+		add_theme_support('automatic-feed-links');
+
+		/* 
+			This denotes that the theme does not set its own 
+			<title> tag, but rather lets WordPress (or plugins)
+			decides what to show.
+
+			The filters below allow us to control aspects
+			of <title> generation from within the theme.
+		*/
+		add_theme_support('title-tag');
+
+		add_filter('document_title_parts', function($title) {
+			// Remove the tagline from the front page
+			unset($title['tagline']);
+			return $title;
 		});
 
-		add_filter('timber_context', function($context) {
-			$context['pagination'] = Timber::get_pagination();
-			$context['main_menu'] = new Timber\Menu('main-menu');
-			$context['footer_menu'] = new Timber\Menu('footer-menu');
-			return $context;
+		add_filter('document_title_separator', function($sep) {
+			return '·';
 		});
-
-		add_action('after_setup_theme', function() {
-			load_theme_textdomain('lathe', get_template_directory() . '/languages');
-		});
-		
-		parent::__construct();
 	}
 	
 }
